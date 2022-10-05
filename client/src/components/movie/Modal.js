d12eeaa4e48d5070c8a5b9bb5e30a31a8c20be9a
JUMTBOX <yjey12@naver.com>
2022-10-05 14:39:08 +0900

    Movie detail page layout Movie페이지의 레이아웃 컴포넌트 작성

---
+++ client/src/components/movie/Modal.js
@@ -0,0 +1,47 @@
+import { Box, Modal, Button, Typography } from '@mui/material';
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const CustomBox = styled(Box)`
+  position: absolute;
+  transform: translate(-50%, -50%);
+  top: 50%;
+  left: 50%;
+  width: 750px;
+  height: 550px;
+  background-color: white;
+  border: 2px solid #000;
+  border-radius: 15px;
+  box-shadow: 24;
+  padding: 4;
+`;
+
+function ActorModal() {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Button onClick={handleOpen}>Modal open</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <CustomBox>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Actor's Modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            해당배우의 다른 출연작
+          </Typography>
+        </CustomBox>
+      </Modal>
+    </div>
+  );
+}
+
+export default ActorModal;
