7998cae22dadeb6e86553268869f55fc13b4ccb3
jungyu <jungyu@naver.com>
2022-10-07 10:30:52 +0900

    feat : Search Page 상단 캐루셀 구현 Search Page 상단 두 줄 캐루셀 레이아웃 구현

---
+++ client/src/components/search/Carousel.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import styled from 'styled-components';
+import PrevArrow from './PrevArrow';
+import NextArrow from './NextArrow';
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 1rem 0;
+`;
+
+const CustomSlicer = styled(Slider)`
+  width: 95%;
+`;
+
+const Image = styled.div`
+  height: 18vh;
+  padding: 10px;
+  background-color: gray;
+`;
+
+function Carousel() {
+  const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    autoplaySpeed: 1000,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+  };
+
+  return (
+    <Wrapper>
+      <CustomSlicer {...settings}>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item, index) => (
+          <Image key={index}>{item}</Image>
+        ))}
+      </CustomSlicer>
+    </Wrapper>
+  );
+}
+
+export default Carousel;
