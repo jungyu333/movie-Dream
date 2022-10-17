222750f3dc48d93ab22963650109945c3252e3f3
jungyu <jungyu@naver.com>
2022-10-17 12:49:08 +0900

    fix : mainSearchService 오타 수정 & index 명 변경 showTimeFilter 오타 수정 & index 명 변경

--- server/src/static/commonStatic.js
+++ server/src/static/commonStatic.js
@@ -1,6 +1,6 @@
 class CommonStatic {
-  static ES_MOVIE_INDEX = 'movie_test';
-  static ES_REVIEW_INDEX = 'review_test';
+  static ES_MOVIE_INDEX = 'movie';
+  static ES_REVIEW_INDEX = 'review';
   static ES_KEYWORD_INDEX = 'review_keyword';
   static ES_MOVIE_AUTO_INDEX = 'auto_complete_movie';
 }
