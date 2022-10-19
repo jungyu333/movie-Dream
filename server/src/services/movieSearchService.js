454ee63e605701a17ef1a3c946b3184ebe4002d0
jungyu <jungyu@naver.com>
2022-10-19 14:08:28 +0900

    style : service 코드 공백 제거 코드 공백 제거

--- server/src/services/movieSearchService.js
+++ server/src/services/movieSearchService.js
@@ -2,31 +2,27 @@ import es from '../lib/elasticsearch.js';
 import common from '../static/commonStatic.js';
 import esb from 'elastic-builder';
 
-
 export default async function getMovie(queryParams, callback) {
+  let query = queryParams.movie_id;
 
-    let query = queryParams.movie_id;
+  //return Value
+  let responseData = {};
 
-    //return Value
-    let responseData = {};
+  const requestBody = new esb.requestBodySearch();
 
-    const requestBody = new esb.requestBodySearch();
+  const bodyData = requestBody
+    .query(esb.matchQuery('movie_id', query))
+    .size(1)
+    .toJSON();
 
-    const bodyData = requestBody
-        .query(esb.matchQuery('movie_id', query))
-        .size(1)
-        .toJSON();
+  const response = await es.search({
+    index: common.ES_MOVIE_INDEX,
+    body: bodyData
+  });
 
-    const response = await es.search({
-        index: common.ES_MOVIE_INDEX,
-        body: bodyData
-    });
+  const hit = response.body.hits.hits[0];
 
-    
-    const hit = response.body.hits.hits[0];
-    
-    responseData['movie'] = hit._source;
+  responseData['movie'] = hit._source;
 
-    callback(false, responseData);
-
+  callback(false, responseData);
 }
