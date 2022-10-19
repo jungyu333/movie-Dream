80db9b359b89f571a69af6a535701c589eaf2bd2
jcy <jcy168942@gmail.com>
2022-10-19 09:16:30 +0900

    movie detail 관련 service추가

---
+++ server/src/services/movieGroupSearchService.js
@@ -0,0 +1,63 @@
+import es from '../lib/elasticsearch.js';
+import common from '../static/commonStatic.js';
+import esb from 'elastic-builder';
+
+
+export default async function getMoiveGroup(queryParams, callback) {
+
+    let group = queryParams.group;
+    let name = queryParams.name; 
+
+    const responseData = {};
+
+    //query
+    const requestBody = new esb.requestBodySearch();
+    const boolQuery = new esb.boolQuery();
+    let bodyData = null;
+
+    if ( group === '감독') {
+        boolQuery.must([
+            esb.matchQuery('movie_director', name),
+        ]);
+
+        bodyData = requestBody.query(boolQuery)
+                              .agg(
+                                    esb.termsAggregation('genre', 'genre')
+                                )
+                               .toJSON();
+    }else {
+        boolQuery.must([
+            esb.matchQuery('movie_actor.name', name),
+        ]);
+        const nestedQuery = new esb.nestedQuery(boolQuery,'movie_actor');
+        bodyData = requestBody.query(nestedQuery)
+                              .agg(
+                                esb.termsAggregation('genre', 'genre')
+                              )
+                              .toJSON();
+    }
+
+    const response = await es.search({
+        index: common.ES_MOVIE_INDEX,
+        body: bodyData
+    });
+
+    const movieList = [];
+
+    for (const hit of response.body.hits.hits) {
+        const hitMap = {};
+        hitMap['movie_id'] = hit._source.movie_id;
+        hitMap['movie_poster'] = hit._source.movie_poster;
+        hitMap['h_movie'] = hit._source.h_movie;
+        movieList.push(hitMap);
+    }
+
+    responseData['group'] = group;
+    responseData['movie'] = movieList;
+
+    //response agg
+    const aggregation = response.body.aggregations;
+    responseData['genre'] = aggregation.genre.buckets;
+
+    callback(false, responseData);
+}
