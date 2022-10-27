c50273469b5cf8aa4d3a67c9f2424a5b44a10079
Yong <jcy168942@gmail.com>
2022-10-27 10:25:20 +0900

    main 페이지 genre별 리스트 api 추가

---
+++ server/src/services/search/genreSearchService.js
@@ -0,0 +1,75 @@
+import es from '../../lib/elasticsearch.js';
+import common from '../../static/commonStatic.js';
+import esb from 'elastic-builder';
+import { multiSearch } from '../../util/esUtil.js';
+
+export default async function getGenreList(callback) {
+    //today
+    let today = new Date();
+    const year = today.getFullYear().toString();
+    const month = ('0' + (today.getMonth() + 1)).slice(-2);
+    const recentMonth = ('0' + (today.getMonth() - 5)).slice(-2);
+    const date = ('0' + today.getDate()).slice(-2);
+
+    today = year + '.' + month.toString() + '.' + date;
+    const recentDay = year + '.' + recentMonth + '.' + date;
+
+    const genreResponse = await genreSearch();
+    const allGenre = [];
+    const agg = genreResponse.body.aggregations;
+    for (const bucket of agg['genre'].buckets) {
+        allGenre.push(bucket['key']);
+    }
+
+    const genreMovieList = [];
+    for (const genre of allGenre) {
+        const genreMap = {};
+        const requestBody = esb.requestBodySearch();
+        const boolQuery = esb.boolQuery();
+        boolQuery
+            .must(esb.matchQuery('genre', genre))
+            .must(esb.rangeQuery('opening_date').gte(recentDay).lte(today));
+        const bodyData = requestBody
+            .query(boolQuery)
+            .sort(esb.sort('opening_date', 'desc'))
+            .toJSON();
+        bodyData['_source'] = [
+            'movie_id',
+            'h_movie',
+            'movie_poster',
+            'genre',
+            'opening_date'
+        ];
+        const response = await es.search({
+            index: common.ES_MOVIE_INDEX,
+            body: bodyData
+        });
+
+        const hits = response.body.hits.hits;
+        const moviesList = [];
+        for (const hit of hits) {
+            const movieMap = hit._source;
+            moviesList.push(movieMap);
+        }
+
+        genreMap['genre'] = genre;
+        genreMap['movies'] = moviesList;
+        genreMovieList.push(genreMap);
+    }
+
+    callback(false, genreMovieList);
+}
+
+async function genreSearch() {
+    const requestBody = new esb.requestBodySearch();
+    const bodyData = requestBody
+        .agg(esb.termsAggregation('genre', 'genre'))
+        .toJSON();
+
+    const response = await es.search({
+        index: common.ES_MOVIE_INDEX,
+        body: bodyData
+    });
+
+    return response;
+}
