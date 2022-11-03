2ea5dfbdcc051a655ffd9b277d7944d56bf9f25d
jungyu <jungyu@naver.com>
2022-11-03 20:58:28 +0900

    feat : 검색어 자동완성 하이라이팅 구현 자동완성 영화 제목과 검색어간 일치하는 부분을 영화 제목 앞쪽부터 하이라이팅 할 수 있도록 구현완료

---
+++ client/src/hooks/createRex.js
@@ -0,0 +1,19 @@
+function createRex(query) {
+  const length = query.length;
+
+  let resultStr = '';
+
+  for (let i = 0; i < length; i++) {
+    const k = query.substr(i, 1);
+
+    if (k === ' ') {
+      continue;
+    }
+
+    resultStr += k + '\\s?';
+  }
+
+  return resultStr;
+}
+
+export default createRex;
