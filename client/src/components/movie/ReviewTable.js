b4edd9f3be71f85e3d41ac5449589be95576032e
jungyu <96876293+jungyu333@users.noreply.github.com>
2022-11-07 11:29:52 +0900

    feat : Review table 제목 , 스크롤창 구현 (#114)
    Review Table 긍정, 부정 소제목 , 각각 리뷰 구역 별로 스크롤링 구현

--- client/src/components/movie/ReviewTable.js
+++ client/src/components/movie/ReviewTable.js
@@ -1,12 +1,15 @@
 import styled from 'styled-components';
 import MultiReview from './MultiReview';
-import NoResult from './NoResult';
 import ReviewItem from './ReviewItem';
 import ReviewNoResult from './ReviewNoResult';
 
 const Wrapper = styled.div`
   width: 100%;
   max-height: 290px;
+`;
+
+const Container = styled.div`
+  height: 290px;
   overflow-y: auto;
   ::-webkit-scrollbar {
     display: none;
@@ -23,11 +26,11 @@ function ReviewTable({ negative, positive, isNegative, isMobile }) {
           {isNegative === 0 ? (
             <>
               {positive.length > 0 ? (
-                <>
+                <Container>
                   {positive.map((review, index) => (
                     <ReviewItem key={index} review={review} />
                   ))}
-                </>
+                </Container>
               ) : (
                 <ReviewNoResult />
               )}
@@ -37,11 +40,11 @@ function ReviewTable({ negative, positive, isNegative, isMobile }) {
           {isNegative === 1 ? (
             <>
               {negative.length > 0 ? (
-                <>
+                <Container>
                   {negative.map((review, index) => (
                     <ReviewItem key={index} review={review} />
                   ))}
-                </>
+                </Container>
               ) : (
                 <ReviewNoResult />
               )}
