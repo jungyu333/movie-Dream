5e5f308f6e36a6bac8c699e544a617b79f8d99b0
jungyu <jungyu@naver.com>
2022-11-08 20:38:26 +0900

    refactor : core delete

--- server/src/lib/elasticsearch.js
+++ server/src/lib/elasticsearch.js
@@ -5,20 +5,18 @@ dotenv.config();
 
 //local elasticsearch connect
 
-/*
 const client = new Client({
-  host: process.env.LOCAL_ELASTIC_URL,
-  node: process.env.LOCAL_ELASTIC_URL,
-  log: "trace",
+    host: process.env.LOCAL_ELASTIC_URL,
+    node: process.env.LOCAL_ELASTIC_URL,
+    log: 'trace'
 });
-*/
 
 //dev elasticsearch connect
 
-const client = new Client({
-    host: process.env.DEV_ELASTIC_URL,
-    node: process.env.DEV_ELASTIC_URL,
-    log: 'trace'
-});
+// const client = new Client({
+//     host: process.env.DEV_ELASTIC_URL,
+//     node: process.env.DEV_ELASTIC_URL,
+//     log: 'trace'
+// });
 
 export default client;
